f99c136aeff357fe1d4063a93fd8562652c33d31
Kestas <k33370@gmail.com>
2022-10-20 01:09:40 +0300

    A little maintenance

--- Papildomai/SumTheseForMe/src/main/java/Program.java
+++ Papildomai/SumTheseForMe/src/main/java/Program.java
@@ -1,9 +1,13 @@
-package lt.vtmc.ks.sumthese;
+
 //©KS
-public class main {
+public class Program {
+	
 	public static int sum(int[] array, int fromWhere, int toWhere, int smallest, int largest) {
-		smallest = (smallest < 0)? 0: smallest;
-		largest = (largest > array.length-1)? array.length-1: largest;
+		
+		fromWhere = fromWhere < 0 ? 0: fromWhere;
+		fromWhere = fromWhere > array.length ? fromWhere = array.length -1 : fromWhere;
+		
+		toWhere = toWhere > array.length ? array.length : toWhere;
 		
 		int sum =0;
 		for (int i = fromWhere; i < toWhere; i++) {
@@ -16,9 +20,9 @@ public class main {
 }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] numbers = {0, 0, 0, 10};
+		int[] numbers = {1};
 		
-		System.out.println(sum(numbers, 0, 0, 0, 0));
+		System.out.println(sum(numbers, -2, 3, 0, 999));
 		
 //		System.out.println(sum(numbers, 0, 0, 0, 0));
 //		System.out.println(sum(numbers, 0, 0, 0, 10));
