cb14d97ee07ca53f0adaf0be8c05b055234140a9
Kestas <k33370@gmail.com>
2022-11-08 18:46:07 +0200

    branch rytas2, Generics box done

---
+++ 12_lambdas_praktinis_1/src/ManoCons.java
@@ -0,0 +1,11 @@
+import java.util.function.Consumer;
+
+public class ManoCons implements Consumer<String> {
+
+	@Override
+	public void accept(String t) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
