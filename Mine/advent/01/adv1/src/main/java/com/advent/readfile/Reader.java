875d6ce98d58503d800497f7769a7c0183127d1b
Kestas <k33370@gmail.com>
2022-12-15 17:40:11 +0200

    Reader back

--- Mine/advent/01/adv1/src/main/java/com/advent/readfile/Reader.java
+++ Mine/advent/01/adv1/src/main/java/com/advent/readfile/Reader.java
@@ -1,4 +1,21 @@
 package com.advent.readfile;
-public class Reader {
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class Reader {
+public ArrayList<String> read(String filename){
+    ArrayList<String> list = new ArrayList<>();
+    File file = new File(filename);
+    try {
+        list.addAll(Files.readAllLines(Paths.get(file.toURI())));
+
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    return list;
+}
 }
